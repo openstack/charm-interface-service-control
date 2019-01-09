323c1c3b14562d7db05648be57418a2871ce26dd
Frode Nordahl <frode.nordahl@canonical.com>
2019-01-09 07:47:47 +0100

    Fixup repo config
    Change-Id: I91d545198a8570d7cacf06576c71f18b06c14df4

---
+++ unit_tests/test_requires.py
@@ -0,0 +1,102 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import unittest
+import mock
+
+import requires
+
+
+_hook_args = {}
+
+
+def mock_hook(*args, **kwargs):
+
+    def inner(f):
+        # remember what we were passed.  Note that we can't actually determine
+        # the class we're attached to, as the decorator only gets the function.
+        _hook_args[f.__name__] = dict(args=args, kwargs=kwargs)
+        return f
+    return inner
+
+
+class TestServiceControlRequires(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls._patched_hook = mock.patch('charms.reactive.hook', mock_hook)
+        cls._patched_hook_started = cls._patched_hook.start()
+        # force requires to rerun the mock_hook decorator:
+        # try except is Python2/Python3 compatibility as Python3 has moved
+        # reload to importlib.
+        try:
+            reload(requires)
+        except NameError:
+            import importlib
+            importlib.reload(requires)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls._patched_hook.stop()
+        cls._patched_hook_started = None
+        cls._patched_hook = None
+        # and fix any breakage we did to the module
+        try:
+            reload(requires)
+        except NameError:
+            import importlib
+            importlib.reload(requires)
+
+    def setUp(self):
+        self.sr = requires.ServiceControlRequires('some-relation', [])
+        self._patches = {}
+        self._patches_start = {}
+
+    def tearDown(self):
+        self.sr = None
+        for k, v in self._patches.items():
+            v.stop()
+            setattr(self, k, None)
+        self._patches = None
+        self._patches_start = None
+
+    def patch_sr(self, attr, return_value=None):
+        mocked = mock.patch.object(self.sr, attr)
+        self._patches[attr] = mocked
+        started = mocked.start()
+        started.return_value = return_value
+        self._patches_start[attr] = started
+        setattr(self, attr, started)
+
+    def test_registered_hooks(self):
+        # test that the hooks actually registered the relation expressions that
+        # are meaningful for this interface: this is to handle regressions.
+        # The keys are the function names that the hook attaches to.
+        hook_patterns = {
+            'broken': (
+                '{requires:service-control}-relation-{broken,departed}', ),
+            'changed': (
+                '{requires:service-control}-relation-{joined,changed}', ),
+        }
+        for k, v in _hook_args.items():
+            self.assertEqual(hook_patterns[k], v['args'])
+
+    def test_broken(self):
+        self.patch_sr('remove_state')
+        self.sr.broken()
+        self.remove_state.assert_called_once_with('{relation_name}.connected')
+
+    def test_changed(self):
+        self.patch_sr('set_state')
+        self.sr.changed()
+        self.set_state.assert_called_once_with('{relation_name}.connected')
